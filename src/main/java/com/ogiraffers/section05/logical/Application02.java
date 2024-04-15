65ddffbe869ef1386a5c6f3e5b14d297913652a8
KHY <power4206@gmail.com>
2024-04-15 18:08:55 +0900

    java20240415

--- src/main/java/com/ogiraffers/section05/logical/Application02.java
+++ src/main/java/com/ogiraffers/section05/logical/Application02.java
@@ -14,19 +14,19 @@ public class Application02 {
         System.out.println("1부터 100사이인지 확인 : " + (num1 >= 1 && num1 <= 100)); //true
 
         int num2 = 166;
-        System.out.println("num2가 1부터 100 사이인지 확인 : " + (num2>=1 && num2<=100));
+        System.out.println("num2가 1부터 100 사이인지 확인 : " + (num2>=1 && num2<=100)); //false
 
         char ch1 ='G';
-        System.out.println("ch1이 대문자인지 확인하기 : " + (ch1 == 'G'));
+        System.out.println("ch1이 대문자인지 확인하기 : " + (ch1 == 'G')); //true
 
         char ch2 = 'Y';
-        System.out.println("영문자 y인지 확인 : " + ((ch2=='y')||(ch2=='Y')));
+        System.out.println("영문자 y인지 확인 : " + ((ch2=='y')||(ch2=='Y'))); //true
 
         char ch3 = 'y';
-        System.out.println("영문자 y인지 확인 : " + ((ch3=='Y')||(ch3=='y')));
+        System.out.println("영문자 y인지 확인 : " + ((ch3=='Y')||(ch3=='y'))); //true
 
         char ch4 = 'f';
-        System.out.println("영문자인지 확인 : " + (((ch4 >= 'a')||(ch4 <= 'z')||(ch4 >= 'A')||(ch4<='Z'))));
+        System.out.println("영문자인지 확인 : " + (((ch4 >= 'a')||(ch4 <= 'z'))||((ch4 >= 'A')||(ch4<='Z')))); //true
 
 
 
